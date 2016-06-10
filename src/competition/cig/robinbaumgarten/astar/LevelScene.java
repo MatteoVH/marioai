8ac15ab3af0c41d61ef02a09b44cd698e25fd7ae
Matteo Vesprini-Heidrich <matteovh@ucla.edu>
2016-06-09 17:01:45 -0700

    add function to dump game state and reward value into q-learner

--- src/competition/cig/robinbaumgarten/astar/LevelScene.java
+++ src/competition/cig/robinbaumgarten/astar/LevelScene.java
@@ -54,6 +54,8 @@ public class LevelScene implements SpriteContext, Cloneable
     public int linesWritten = 0;
 
     public static int rowLabel = 0;
+
+    public float lastXCamPos = 0;
     
     public static List<Sprite> cloneList(List<Sprite> list) throws CloneNotSupportedException {
         List<Sprite> clone = new ArrayList<Sprite>(list.size());
@@ -273,6 +275,131 @@ public class LevelScene implements SpriteContext, Cloneable
         //System.out.println("position calculated: " + (xPosRelativeToCamera + yBoost));
 
         return xPosRelativeToCamera + yBoost;
+    }{
+        if(mario.mapX < 30 || mario.mapX > 140)
+            return;
+        int[] gameState = new int[LEARNING_ARRAY_COLUMNS * LEARNING_ARRAY_ROWS + LEARNING_ARBITRARY_DIMENSIONS];
+
+        //zero out entire array
+        for(int x = 0; x < gameState.length; x++)
+            gameState[x] = 0;
+
+        //System.out.println(mario.mapX);
+        //System.out.println(level.width);
+
+        for (int x = mario.mapX - 5; x < mario.mapX + 12; x++) {
+            for (int y = 0; y < level.height; y++) {
+                int blockType = level.getBlock(x, y);
+                //System.out.println(blockType);
+                int learnArrayPos = getLearningArrayPosFromCoord(x, y);
+                gameState[learnArrayPos] = blockType;
+//                System.out.println("block x: " + x + ", y: "+y+ "= " + level.getBlock(x, y));
+            }
+        }
+
+        for (Sprite sprite: sprites) {
+            int learnArrayPos = getLearningArrayPos(sprite);
+            if (sprite != mario)
+                gameState[learnArrayPos] = sprite.kind;
+        }
+
+        //this is the end of the grid detection around mario and begins arbitrary parameter insertion into data array
+        int LEARNING_ARRAY_ARBITRARY_PARAM_START = LEARNING_ARRAY_COLUMNS * LEARNING_ARRAY_ROWS;
+
+        //encode mario's ability to jump
+        gameState[LEARNING_ARRAY_ARBITRARY_PARAM_START] = mario.mayJump() ? 1 : 0;
+
+        //encode mario's state
+        if(mario.large) //mario is large
+            gameState[LEARNING_ARRAY_ARBITRARY_PARAM_START + 1] = 1;
+        else if(mario.fire) //mario is on fire
+            gameState[LEARNING_ARRAY_ARBITRARY_PARAM_START + 1] = 2;
+        else //mario is not large or on fire
+            gameState[LEARNING_ARRAY_ARBITRARY_PARAM_START + 1] = 0;
+
+        //encode mario's x and y velocity
+        gameState[LEARNING_ARRAY_ARBITRARY_PARAM_START + 2] = (int) mario.xa;
+        gameState[LEARNING_ARRAY_ARBITRARY_PARAM_START + 3] = (int) mario.ya;
+
+        //encode mario's x distance in the level
+        gameState[LEARNING_ARRAY_ARBITRARY_PARAM_START + 4] = (int) mario.x;
+
+        String line = "" + rowLabel++ + ", ";
+        for (int x = 0; x < gameState.length; x++) {
+            line += gameState[x];
+            if (x != gameState.length - 1)
+                line += ", ";
+            else
+                line += "\n";
+        }
+
+        try {
+            fw.write(line);
+            linesWritten++;
+        } catch (IOException e) {
+            System.out.println("Failed to write");
+        }
+        if (linesWritten > 1000) {
+            try {
+                fw.close();
+            } catch (IOException e) {
+                System.out.println("couldn't close" + e);
+            }
+        }
+    }
+
+    public void dumpStateAndReward() {
+        if(mario.mapX < 30 || mario.mapX > 140)
+            return;
+        int[] gameState = new int[LEARNING_ARRAY_COLUMNS * LEARNING_ARRAY_ROWS + LEARNING_ARBITRARY_DIMENSIONS];
+
+        //zero out entire array
+        for(int x = 0; x < gameState.length; x++)
+            gameState[x] = 0;
+
+        //System.out.println(mario.mapX);
+        //System.out.println(level.width);
+
+        for (int x = mario.mapX - 5; x < mario.mapX + 12; x++) {
+            for (int y = 0; y < level.height; y++) {
+                int blockType = level.getBlock(x, y);
+                //System.out.println(blockType);
+                int learnArrayPos = getLearningArrayPosFromCoord(x, y);
+                gameState[learnArrayPos] = blockType;
+//                System.out.println("block x: " + x + ", y: "+y+ "= " + level.getBlock(x, y));
+            }
+        }
+
+        for (Sprite sprite: sprites) {
+            int learnArrayPos = getLearningArrayPos(sprite);
+            if (sprite != mario)
+                gameState[learnArrayPos] = sprite.kind;
+        }
+
+        //this is the end of the grid detection around mario and begins arbitrary parameter insertion into data array
+        int LEARNING_ARRAY_ARBITRARY_PARAM_START = LEARNING_ARRAY_COLUMNS * LEARNING_ARRAY_ROWS;
+
+        //encode mario's ability to jump
+        gameState[LEARNING_ARRAY_ARBITRARY_PARAM_START] = mario.mayJump() ? 1 : 0;
+
+        //encode mario's state
+        if(mario.large) //mario is large
+            gameState[LEARNING_ARRAY_ARBITRARY_PARAM_START + 1] = 1;
+        else if(mario.fire) //mario is on fire
+            gameState[LEARNING_ARRAY_ARBITRARY_PARAM_START + 1] = 2;
+        else //mario is not large or on fire
+            gameState[LEARNING_ARRAY_ARBITRARY_PARAM_START + 1] = 0;
+
+        //encode mario's x and y velocity
+        gameState[LEARNING_ARRAY_ARBITRARY_PARAM_START + 2] = (int) mario.xa;
+        gameState[LEARNING_ARRAY_ARBITRARY_PARAM_START + 3] = (int) mario.ya;
+
+        //encode mario's x distance in the level
+        gameState[LEARNING_ARRAY_ARBITRARY_PARAM_START + 4] = (int) mario.x;
+
+        float reward = mario.x - mario.xOld;
+
+        NNSimulator.updateAction(gameState, reward);
     }
 
     //dump all the enemy
